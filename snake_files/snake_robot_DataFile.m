c228f85f1a93e5e96c9930c077407bd7e5b11181
Maria Luisa Scarpa <80356415+mls18@users.noreply.github.com>
2025-10-07 12:39:35 +0200

    Add files via upload

---
+++ snake_files/snake_robot_DataFile.m
@@ -0,0 +1,128 @@
+% Simscape(TM) Multibody(TM) version: 24.2
+
+% This is a model data file derived from a Simscape Multibody Import XML file using the smimport function.
+% The data in this file sets the block parameter values in an imported Simscape Multibody model.
+% For more information on this file, see the smimport function help page in the Simscape Multibody documentation.
+% You can modify numerical values, but avoid any other changes to this file.
+% Do not add code to this file. Do not edit the physical units shown in comments.
+
+%%%VariableName:smiData
+
+
+%============= RigidTransform =============%
+
+%Initialize the RigidTransform structure array by filling in null values.
+smiData.RigidTransform(9).translation = [0.0 0.0 0.0];
+smiData.RigidTransform(9).angle = 0.0;
+smiData.RigidTransform(9).axis = [0.0 0.0 0.0];
+smiData.RigidTransform(9).ID = "";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(1).translation = [0.78740157480315076 2.7559055118110236 0];  % in
+smiData.RigidTransform(1).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(1).axis = [-0.57735026918962584 -0.57735026918962584 0.57735026918962584];
+smiData.RigidTransform(1).ID = "B[TEST_SNAKE_BRACKET-1:-:TEST_SNAKE_BRACKET-2]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(2).translation = [-1.1102230246251565e-16 0.78740157480315409 0.78740157480315154];  % in
+smiData.RigidTransform(2).angle = 3.1415926535897931;  % rad
+smiData.RigidTransform(2).axis = [1 -1.4166602986635615e-33 -3.1398607659963658e-16];
+smiData.RigidTransform(2).ID = "F[TEST_SNAKE_BRACKET-1:-:TEST_SNAKE_BRACKET-2]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(3).translation = [0.78740157480315087 2.7559055118110241 0];  % in
+smiData.RigidTransform(3).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(3).axis = [-0.57735026918962584 -0.57735026918962584 0.57735026918962584];
+smiData.RigidTransform(3).ID = "B[TEST_SNAKE_BRACKET-2:-:TEST_SNAKE_BRACKET-3]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(4).translation = [-9.2287288921966137e-16 0.78740157480314998 -0.78740157480315121];  % in
+smiData.RigidTransform(4).angle = 5.6193922475983175e-16;  % rad
+smiData.RigidTransform(4).axis = [-0.43940605842681535 -0.89828854819473791 1.1090247951034529e-16];
+smiData.RigidTransform(4).ID = "F[TEST_SNAKE_BRACKET-2:-:TEST_SNAKE_BRACKET-3]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(5).translation = [0.78740157480315087 2.7559055118110236 0];  % in
+smiData.RigidTransform(5).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(5).axis = [-0.57735026918962584 -0.57735026918962584 0.57735026918962584];
+smiData.RigidTransform(5).ID = "B[TEST_SNAKE_BRACKET-3:-:TEST_SNAKE_BRACKET-4]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(6).translation = [1.5543122344752192e-15 0.78740157480314676 0.78740157480315076];  % in
+smiData.RigidTransform(6).angle = 3.1415926535897931;  % rad
+smiData.RigidTransform(6).axis = [1 1.3104829023640765e-34 1.6696843510115952e-18];
+smiData.RigidTransform(6).ID = "F[TEST_SNAKE_BRACKET-3:-:TEST_SNAKE_BRACKET-4]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(7).translation = [0.78740157480315032 2.7559055118110205 0];  % in
+smiData.RigidTransform(7).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(7).axis = [-0.57735026918962584 -0.57735026918962584 0.57735026918962584];
+smiData.RigidTransform(7).ID = "B[TEST_SNAKE_BRACKET-4:-:TEST_SNAKE_BRACKET-5]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(8).translation = [4.5102810375396984e-16 0.78740157480315187 -0.78740157480315276];  % in
+smiData.RigidTransform(8).angle = 3.0693033927054138e-16;  % rad
+smiData.RigidTransform(8).axis = [-0.18249123396986039 -0.98320748040490302 2.7535756065321946e-17];
+smiData.RigidTransform(8).ID = "F[TEST_SNAKE_BRACKET-4:-:TEST_SNAKE_BRACKET-5]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(9).translation = [-0.47037502100147377 0.086859910710299984 3.6595300476740862];  % in
+smiData.RigidTransform(9).angle = 0;  % rad
+smiData.RigidTransform(9).axis = [0 0 0];
+smiData.RigidTransform(9).ID = "RootGround[TEST_SNAKE_BRACKET-1]";
+
+
+%============= Solid =============%
+%Center of Mass (CoM) %Moments of Inertia (MoI) %Product of Inertia (PoI)
+
+%Initialize the Solid structure array by filling in null values.
+smiData.Solid(1).mass = 0.0;
+smiData.Solid(1).CoM = [0.0 0.0 0.0];
+smiData.Solid(1).MoI = [0.0 0.0 0.0];
+smiData.Solid(1).PoI = [0.0 0.0 0.0];
+smiData.Solid(1).color = [0.0 0.0 0.0];
+smiData.Solid(1).opacity = 0.0;
+smiData.Solid(1).ID = "";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(1).mass = 0.042006352107659961;  % kg
+smiData.Solid(1).CoM = [0 36.230027464886888 0];  % mm
+smiData.Solid(1).MoI = [27.377581219210192 12.55494606907291 31.808915049979113];  % kg*mm^2
+smiData.Solid(1).PoI = [0 0 0];  % kg*mm^2
+smiData.Solid(1).color = [0.13725490196078433 0.61960784313725492 0];
+smiData.Solid(1).opacity = 1;
+smiData.Solid(1).ID = "TEST_SNAKE_BRACKET*:*Default";
+
+
+%============= Joint =============%
+%X Revolute Primitive (Rx) %Y Revolute Primitive (Ry) %Z Revolute Primitive (Rz)
+%X Prismatic Primitive (Px) %Y Prismatic Primitive (Py) %Z Prismatic Primitive (Pz) %Spherical Primitive (S)
+%Constant Velocity Primitive (CV) %Lead Screw Primitive (LS)
+%Position Target (Pos)
+
+%Initialize the RevoluteJoint structure array by filling in null values.
+smiData.RevoluteJoint(4).Rz.Pos = 0.0;
+smiData.RevoluteJoint(4).ID = "";
+
+smiData.RevoluteJoint(1).Rz.Pos = 91.646522545317197;  % deg
+smiData.RevoluteJoint(1).ID = "[TEST_SNAKE_BRACKET-1:-:TEST_SNAKE_BRACKET-2]";
+
+smiData.RevoluteJoint(2).Rz.Pos = -88.166252868015874;  % deg
+smiData.RevoluteJoint(2).ID = "[TEST_SNAKE_BRACKET-2:-:TEST_SNAKE_BRACKET-3]";
+
+smiData.RevoluteJoint(3).Rz.Pos = 92.437387170211494;  % deg
+smiData.RevoluteJoint(3).ID = "[TEST_SNAKE_BRACKET-3:-:TEST_SNAKE_BRACKET-4]";
+
+smiData.RevoluteJoint(4).Rz.Pos = -93.251670278126653;  % deg
+smiData.RevoluteJoint(4).ID = "[TEST_SNAKE_BRACKET-4:-:TEST_SNAKE_BRACKET-5]";
+
